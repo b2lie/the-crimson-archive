25bc9ec7152a26e1896064c500602a90caa7918a
batool <batoolmao@gmail.com>
2025-11-25 21:07:08 +0500

    fixed map edit button

--- components/mob-browser.tsx
+++ components/mob-browser.tsx
@@ -5,6 +5,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { RefreshCw, Edit2, Trash2 } from "lucide-react"
+import { MobEditModal } from "./mob-edit-modal"
 
 interface Mob {
   mobid: number;
